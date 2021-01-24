8212efb3126e2ca339378c1dcb51ab83b4ae701c
Mohammed Mushahid Qureshi <mushahidq@gmail.com>
2021-01-24 17:54:34 +0530

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D player;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.5f;
+
+    public float runSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+        player.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
---
+++ Assets/Scripts/VirusGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusGenerator : MonoBehaviour
+{
+
+    float x = 0;
+    float y = 0;
+    Vector2 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = (int) Random.Range(0, 10);
+        pos = new Vector2(x, y);
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
